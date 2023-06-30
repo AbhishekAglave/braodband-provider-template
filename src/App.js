57f613ea137c1ecfacf7343cb10b70c1d41ac085
Abhishek <abhishekaglave@grampwoer.com>
2023-06-30 21:55:56 +0530

    first app commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Phone, Mail } from "react-feather";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="shop-name-container">
+          <h1 className="shop-name">M A LINE</h1>
+          <h4 className="service-type">BROADBAND SERCICES</h4>
+        </div>
+        <div className="logo-container">
+          <img src="/Airtel_Xstream_logo.png" alt="brand_image" />
+        </div>
       </header>
+      <main className="App-main">
+        <div className="plans-container">
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+      <footer className="App-footer">
+        <div className="address-container">
+          <div className="footer-label">Address</div>
+          <a
+            href="https://goo.gl/maps/JcHvxjPQwG6zkdrr5"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+            3rd Floor, Arihant Plaza, Amrapali Marg, opp. Crown Plaza, above
+            Indian overseas bank, Block F, Vaishali Nagar, Jaipur, Rajasthan
+            302021
+          </a>
+        </div>
+        <div className="contact-container">
+          <div className="footer-label">Contact</div>
+          <a
+            href="tel:+916261392766"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+            <Phone /> 62613 92766
+          </a>
+          <a
+            href="https://wa.me/916261392766"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+            <Phone /> 62613 92766
+          </a>
+          <a
+            href="mailto:email@example.com"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+            <Mail /> email@example.com
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
 
 export default App;
+// Customer ko apun ka text msg karna hai attatime sab ko jana ohh msg
+// contact ko apun ke offer ke bare mai msg karna
